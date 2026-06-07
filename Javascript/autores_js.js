9de1d0aacf81d1c031451816a24f42e064f5718c
Saul848 <zS24013398@estudiantes.uv.mx>
2026-06-07 14:54:25 -0600

    commit 4

---
+++ Javascript/autores_js.js
@@ -0,0 +1,75 @@
+
+fetch('../XML/biblioteca.xml')
+    .then(response => response.text())
+    .then(str => {
+        const parser = new DOMParser();
+        const xmlBiblioteca = parser.parseFromString(str, "text/xml");
+
+        llenarTablaAutores(xmlBiblioteca);
+    });
+
+function llenarTablaAutores(biblioteca){
+    const cuerpoTabla = document.getElementById('cuerpo-tabla');
+    cuerpoTabla.innerHTML = '';
+
+    const autores = Array.from(biblioteca.getElementsByTagName('autor')).filter(autor => {
+        return autor.getElementsByTagName('nombre').length > 0;
+    });
+
+    cuerpoTabla.innerHTML = '';
+
+    for(let autor of autores){
+        console.log(autor);
+        const nombre = autor.getElementsByTagName('nombre')[0].textContent;
+        const nacionalidad = autor.getElementsByTagName('nacionalidad')[0].textContent;
+
+        agregarFilaATabla(nombre, nacionalidad);
+    }
+    //cargar los autores en localStorage    
+    const autoresLocales = JSON.parse(localStorage.getItem('misAutores')) || [];
+
+    autoresLocales.forEach(autor => {
+        agregarFilaATabla(autor.nombre, autor.nacionalidad);
+    });
+}
+
+function agregarFilaATabla(nombre, nacionalidad) {
+    const cuerpoTabla = document.getElementById('cuerpo-tabla');
+    const fila = document.createElement('tr');
+    fila.innerHTML = `<td>${nombre}</td><td>${nacionalidad}</td>`;
+    cuerpoTabla.appendChild(fila);
+}
+
+function agregarAutor(){
+    const nombreInput = document.getElementById('nombre').value;
+    const nacionalidadInput = document.getElementById('nacionalidad').value;
+
+    if (nombreInput === "" || nacionalidadInput === "") {
+        alert("Por favor, completa ambos campos.");
+        return; 
+    }
+
+    //Lista de autores existentes en localStorage
+    let listaAutores = JSON.parse(localStorage.getItem('misAutores')) || [];
+    //Nuevo autor que se agregará a esta lista
+    listaAutores.push({ nombre: nombreInput, nacionalidad: nacionalidadInput });
+    //Guardar de nuevo en localStorage convertido en texto
+    localStorage.setItem('misAutores', JSON.stringify(listaAutores));
+
+    const tabla = document.getElementById('cuerpo-tabla');
+    console.log("Cuerpo de tabla encontrado:", tabla);
+    const newFila = document.createElement('tr');
+
+    newFila.innerHTML = `<td>${nombreInput}</td><td>${nacionalidadInput}</td>`;
+
+    tabla.appendChild(newFila);
+    //Limpiar la seccion del formulario 
+    document.getElementById('formulario-nuevo-actor').reset();
+}
+
+function limpiarAutores() {
+    // 1. Borramos del almacenamiento
+    localStorage.removeItem('misAutores');
+    
+    location.reload(); 
+}
